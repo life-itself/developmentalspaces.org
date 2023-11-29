2cbd8bb9b5fc7773ac42179f52561be9f79550e8
Rufus Pollock <rufus.pollock@datopian.com>
2023-11-29 21:37:17 +0100

    [flower/config][s]: update config with current info.
    - default to white theme
    - set site title
    - not much else

--- content/config.mjs
+++ content/config.mjs
@@ -1,1 +1,12 @@
-export default {};
+export default {
+  title: '🌱 Transformational Spaces',
+  author: 'Life Itself',
+  authorLogo: 'https://lifeitself.org/assets/site/life-itself-logo.svg',
+  authorUrl: 'https://lifeitself.org/',
+  analytics: 'G-CN5D9D57PC',
+  navLinks: [
+  ],
+  theme: {
+    default: ''
+  }
+};
